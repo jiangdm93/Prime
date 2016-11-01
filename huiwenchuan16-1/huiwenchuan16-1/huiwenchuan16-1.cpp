e1dcee67af91f8f97b6e89834dc1c1f0d6d44932
jiangdm93 <jiangdm1993@qq.com>
2016-11-01 16:56:41 +0800

    实现回文串判断，利用STL方法

--- huiwenchuan16-1/huiwenchuan16-1/huiwenchuan16-1.cpp
+++ huiwenchuan16-1/huiwenchuan16-1/huiwenchuan16-1.cpp
@@ -2,10 +2,32 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
 
-
+bool judge(const std::string & s);
 int main()
 {
-    return 0;
+	std::string s;
+	std::cout << "Please enter a string (enter quit to quit):\n";
+	while ( std::cin >> s && s != "quit" )
+	{
+		if (judge(s) == true)
+			std::cout << "this string is huiwenchuan\n";
+		else std::cout << "this string is not huiwenchuan\n";
+		std::cout << "test again\n";
+	}
+	return 0;
 }
 
+bool judge(const std::string & s)
+{
+	std::string ss = s;
+	//std::cout << "ss is:";
+	reverse(ss.begin(),ss.end());
+	//std::cout << ss << std::endl;
+	if (s.compare(ss) == 0)
+		return true;
+	else return false;
+}
