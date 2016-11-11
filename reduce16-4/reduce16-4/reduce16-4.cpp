e9353e4879a5e70d9df3e329fc63ac495aedf1dd
jiangdm93 <jiangdm1993@qq.com>
2016-11-11 10:40:18 +0800

    改成了模板函数

--- reduce16-4/reduce16-4/reduce16-4.cpp
+++ reduce16-4/reduce16-4/reduce16-4.cpp
@@ -5,23 +5,26 @@
 #include <iostream>
 #include <algorithm>
 
-int reduce(long ar[], int n);
-void show(long x) { std::cout.width(6); std::cout << x << "  "; }
+template<class T>
+int reduce(T ar[], int n);
+template<class T>
+void show(T x) { std::cout.width(6); std::cout << x << "  "; }
 int main()
 {
-	long aa[] = { 5,7,3,9,10,54,1,3,4,5,88,5,4,3 };
-	std::for_each(aa,aa+14,show);
+	double aa[] = { 5.1,7,3.125,9,10,54,1,3,4,5,88,5.8,4,3.125 };
+	std::for_each(aa,aa+14,show<double>);
 	std::cout << std::endl;
 	int num = reduce(aa, 14);
-	std::for_each(aa, aa + num, show);
+	std::for_each(aa, aa + num, show<double>);
 	std::cout << std::endl;
 	system("pause");
     return 0;
 }
 
-int reduce(long ar[], int n)
+template<class T>
+int reduce(T ar[], int n)
 {
 	std::sort(ar,ar+n);
-	long *p = std::unique(ar, ar + n);
+	T *p = std::unique(ar, ar + n);
 	return p - ar;
 }
